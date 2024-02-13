cb23f4bb9f851117b280deb51e4b36cd1eb7a4ff
seung-heee <tmdgmldi2595@naver.com>
2024-02-14 01:25:56 +0900

    Feat: timeTable Page

--- src/pages/PresentList.js
+++ src/pages/PresentList.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import FixedBottom from '../components/FixedBottom';
+import BackBox from '../components/BackBox';
 
 const PresentList = () => {
     return (
-        <div>
+        <div className='PresentList'>
+            <BackBox />
+            <div className="titleFont  text-5xl text-center pt-16 mb-6">경품 목록</div>
             
+            <FixedBottom />
         </div>
     );
 };
