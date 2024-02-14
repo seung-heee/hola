05d29b4a561e007336e75017138247f9e829d003
seung-heee <tmdgmldi2595@naver.com>
2024-02-14 12:07:09 +0900

    ?? "

--- src/components/TimeTableElement.js
+++ src/components/TimeTableElement.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import { LuMousePointerClick } from "react-icons/lu";
+import { Link } from 'react-router-dom';
+
 
 const TimeTableElement = ({time, schedule}) => {
     return (
         <>
             <div>{time}</div>
-            <div>{schedule}</div>
+            { schedule === 'Secret Guest' ?
+                <Link to='/guestPage' className='flex items-center'>
+                    <div className='italic underline'>{schedule}</div>
+                    <LuMousePointerClick className='ml-1'/>
+                </Link> :
+                <div>{schedule}</div>
+            }
         </>
    
     );
