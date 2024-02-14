695aaa6922de9c7a7b901fbf0d0a79c162ea345d
seung-heee <tmdgmldi2595@naver.com>
2024-02-14 16:40:53 +0900

    Design: Add backbox

--- src/pages/PresentList.js
+++ src/pages/PresentList.js
@@ -7,29 +7,31 @@ const PresentList = () => {
     return (
         <div className='PresentList subtitleFont relative'>
             <BackBox />
-            <div className="text-6xl text-center pt-16 mb-10 tracking-widest titleFont">경품 목록</div>
-        
-            <div className='text-2xl flex flex-col w-9/12 mx-auto relative mb-52'>
-                <div className='text-start'>
-                    <div>[1등]<br/>2022 IPAD<br/>10세대</div>
+            <div className='relative z-10'>
+                <div className="text-6xl text-center pt-16 mb-10 tracking-widest titleFont">경품 목록</div>
+            
+                <div className='text-2xl flex flex-col w-9/12 mx-auto relative mb-52'>
+                    <div className='text-start'>
+                        <div>[1등]<br/>2022 IPAD<br/>10세대</div>
+                    </div>
+                    <img src={images.ipad} alt="ipad" className='w-8/12 absolute -bottom-40 -right-4'/>
                 </div>
-                <img src={images.ipad} alt="ipad" className='w-8/12 absolute -bottom-40 -right-4'/>
-            </div>
 
-            <div className='text-2xl flex flex-col w-9/12 mx-auto relative mb-52'>
-                <div className='text-end'>
-                    <div>[2등]<br/>SONY 헤드폰<br/>WH-<br/>1000XMS</div>
+                <div className='text-2xl flex flex-col w-9/12 mx-auto relative mb-52'>
+                    <div className='text-end'>
+                        <div>[2등]<br/>SONY 헤드폰<br/>WH-<br/>1000XMS</div>
+                    </div>
+                    <img src={images.sony} alt="ipad" className='w-8/12 absolute -bottom-44 -left-4'/>
                 </div>
-                <img src={images.sony} alt="ipad" className='w-8/12 absolute -bottom-44 -left-4'/>
-            </div>
 
-            <div className='text-2xl flex flex-col w-9/12 mx-auto relative mb-52'>
-                <div className='text-start'>
-                    <div>[3등]<br/>APPLE Watch<br/>SE 2</div>
+                <div className='text-2xl flex flex-col w-9/12 mx-auto relative mb-52'>
+                    <div className='text-start'>
+                        <div>[3등]<br/>APPLE Watch<br/>SE 2</div>
+                    </div>
+                    <img src={images.appleWatch} alt="ipad" className='w-8/12 absolute -bottom-40 -right-4'/>
                 </div>
-                <img src={images.appleWatch} alt="ipad" className='w-8/12 absolute -bottom-40 -right-4'/>
+                <FixedBottom />
             </div>
-            <FixedBottom />
         </div>
     );
 };
--- src/pages/SnsEvent.js
+++ src/pages/SnsEvent.js
@@ -1,27 +1,31 @@
 import React from 'react';
 import FixedBottom from '../components/FixedBottom';
 import { images } from '../utils/images';
+import BackBox from '../components/BackBox';
 
 const SnsEvent = () => {
     return (
         <div className='SnsEvent relative textRixFont'>
-            <div className="titleFont text-6xl text-center pt-16 mb-6">SNS 이벤트</div>
+            <BackBox />
+            <div className='relative z-10'>
+                <div className="titleFont text-6xl text-center pt-16 mb-6">SNS 이벤트</div>
 
-            <div className='textRixFont eventBox text-xl text-center w-10/12 mx-auto break-keep orange'>
-                릴스, 필터 이벤트를 통해서 각 이벤트 당 <span className='text-white'>학식쿠폰 50장</span>을 드립니다!
-            </div>
-            
-            <div className='my-4 flex w-10/12 mx-auto justify-center items-center text-center text-xl'>
-                <div className='w-6/12'><img src={images.sns1} alt="" /></div>
-                <div className='whitespace-nowrap'>MZ 새내기들을 위한<br/>다함께<br/>
-                <span className='textRixFontOrange'>NEW:START</span><br/> 릴스 챌린지!</div>
-            </div>
+                <div className='textRixFont eventBox text-xl text-center w-10/12 mx-auto break-keep orange'>
+                    릴스, 필터 이벤트를 통해서 각 이벤트 당 <span className='text-white'>학식쿠폰 50장</span>을 드립니다!
+                </div>
+                
+                <div className='my-4 flex w-10/12 mx-auto justify-center items-center text-center text-xl'>
+                    <div className='w-6/12'><img src={images.sns1} alt="" /></div>
+                    <div className='whitespace-nowrap'>MZ 새내기들을 위한<br/>다함께<br/>
+                    <span className='textRixFontOrange'>NEW:START</span><br/> 릴스 챌린지!</div>
+                </div>
 
-            <div className='my-4 flex flex-row-reverse w-10/12 mx-auto justify-center items-center text-center text-xl'>
-                <div className='w-6/12'><img src={images.sns2} alt="" /></div>
-                <div className='whitespace-nowrap'>MZ 새내기들을 위한<br/><span className='textRixFontOrange'>HOLA! PICTURE</span> <br/> 이벤트!</div>
+                <div className='my-4 flex flex-row-reverse w-10/12 mx-auto justify-center items-center text-center text-xl'>
+                    <div className='w-6/12'><img src={images.sns2} alt="" /></div>
+                    <div className='whitespace-nowrap'>MZ 새내기들을 위한<br/><span className='textRixFontOrange'>HOLA! PICTURE</span> <br/> 이벤트!</div>
+                </div>
+                <FixedBottom />
             </div>
-            <FixedBottom />
         </div>
     );
 };
