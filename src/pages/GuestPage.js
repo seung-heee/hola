d0303e8e76912493c3bb11e889c38a44d226b5ef
seung-heee <tmdgmldi2595@naver.com>
2024-02-16 12:45:49 +0900

    fix: og

--- src/pages/GuestPage.js
+++ src/pages/GuestPage.js
@@ -17,9 +17,9 @@ const GuestPage = () => {
             
             <div className='subtitleFont flex flex-col w-10/12 mx-auto text-center text-xl break-keep'>
               <div className='guestText'>성결대학교 신입생 여러분을 위한 시크릿 게스트의 무대!!</div>
-              <div className='w-6/12 mx-auto'><img src={images.guest1} alt="guest1" /></div>
+              <div className='w-6/12 mx-auto slide-in-right'><img src={images.guest1} alt="guest1" /></div>
               <div className='guestText1'>실루엣을 보고 누구인지 맞춰보세요!</div>
-              <div className='w-6/12 mx-auto'><img src={images.guest2} alt="guest2" /></div>
+              <div className='w-6/12 mx-auto slide-in-left '><img src={images.guest2} alt="guest2" /></div>
             </div>
             <FixedBottom />
             </div>
